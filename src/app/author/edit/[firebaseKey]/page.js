4558e20dd374f5a40e52699758df17c5acbc9ab6
Tyler Davenport <tydavenport0415@gmail.com>
2025-01-11 18:38:20 -0600

    update complete

--- src/app/author/edit/[firebaseKey]/page.js
+++ src/app/author/edit/[firebaseKey]/page.js
@@ -1,13 +1,14 @@
+'use client';
+
 import React, { useEffect, useState } from 'react';
-import { useRouter } from 'next/router';
+import PropTypes from 'prop-types';
 import { getSingleAuthor } from '../../../../api/authorData';
 import AuthorForm from '../../../../components/forms/AuthorForm';
 
-export default function EditAuthor() {
+export default function EditAuthor({ params }) {
   const [editItem, setEditItem] = useState({});
-  const router = useRouter();
   // TODO: grab the firebasekey
-  const { firebaseKey } = router.query;
+  const { firebaseKey } = params;
 
   // TODO: make a call to the API to get the book data
   useEffect(() => {
@@ -17,3 +18,7 @@ export default function EditAuthor() {
   // TODO: pass object to form
   return <AuthorForm obj={editItem} />;
 }
+
+EditAuthor.propTypes = {
+  params: PropTypes.objectOf({}).isRequired,
+};
